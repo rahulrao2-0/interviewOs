f478604452572378917f342c4fe1c4ce421a9c5a
Rahul Yadav <yadavrahul81135@gmail.com>
2026-06-01 09:50:18 +0530

    Update cors

--- Backend/app.js
+++ Backend/app.js
@@ -20,11 +20,27 @@ import interviewerDashboardRoutes from "./routes/interviewerDashboard.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+import cors from "cors";
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://interview-os-ten.vercel.app"
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"]
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
 }));
 
 const server = createServer(app);
