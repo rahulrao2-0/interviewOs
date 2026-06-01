d0d1d762aba28550cc89c7c6f9fc279f66b2609e
Rahul Yadav <yadavrahul81135@gmail.com>
2026-06-01 09:46:51 +0530

    make corrections

--- Forntend/src/HomePage/MainPage.jsx
+++ Forntend/src/HomePage/MainPage.jsx
@@ -1,5 +1,5 @@
 import Header from "./Header.jsx"
-import MainContainer from "./mainContainer.jsx"
+import MainContainer from "./MainContainer.jsx"
 import Toolbar from '@mui/material/Toolbar';
 import Footer from "./Footer.jsx";
 export function MainPage (){
