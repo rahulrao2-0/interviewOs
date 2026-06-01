75b46563260ea59f6940938a43317b0c1574d5f0
Rahul Yadav <yadavrahul81135@gmail.com>
2026-06-01 09:54:43 +0530

    .

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ import interviewerDashboardRoutes from "./routes/interviewerDashboard.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-import cors from "cors";
+
 
 const allowedOrigins = [
   "http://localhost:5173",
