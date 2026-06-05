cb9f2e62bb52b542cee4a4965f9cf42ffd5d1c79
Rahul Yadav <yadavrahul81135@gmail.com>
2026-06-05 10:52:17 +0530

    final check

--- Forntend/src/Auth/Login.jsx
+++ Forntend/src/Auth/Login.jsx
@@ -79,7 +79,7 @@ export default function Login() {
       setError(res.message || "Login failed");
     }
 
-    if(res.user.profileExist===1){
+    if(res.user===1){
       navigate("/");
     }else{
       navigate("/profileSetup", {
